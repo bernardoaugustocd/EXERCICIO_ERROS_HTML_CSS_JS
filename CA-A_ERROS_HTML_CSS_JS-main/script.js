a4201976b0b1e4293b2bee226d4a242ae8b6e285
SITE_JURIDICO <bernardoaugustocd@gmail.com>
2026-01-28 19:52:06 -0300

    EXERCICIO_ERROS_HTML_CSS_JS|Bernardo: Correção de erros no html, java script e css. E verificação de execução do site

---
+++ CA-A_ERROS_HTML_CSS_JS-main/script.js
@@ -0,0 +1,36 @@
+function senha() {
+
+    var usuario, senha, mensagem;
+
+    usuario = document.getElementById("usuario").value;
+    senha = document.getElementById("inpSenha").value;
+    mensagem = document.getElementById("pMensagem");
+
+    //se a dropdownlist não estiver selecionada
+    if (usuario === "0") {
+        mensagem.innerHTML = "Selecione o tipo de usuário!";
+    }
+
+    // Usuário comum
+    else if (usuario === "1" && senha === "123") {
+        alert("Você será direcionado para a página de conta bancaria");
+        window.location.href = "contaBancaria.html";
+    }
+
+    // Conta salário
+    else if (usuario === "2" && senha === "456") {
+        alert("Você será direcionado para a página de conta salário.");
+        window.location.href = "contaSalario.html";
+    }
+
+    // Aposentado
+    else if (usuario === "3" && senha === "789") {
+        alert("Você será direcionado para a página de benefício aposentadoria.");
+        window.location.href = "aposentado.html";
+    }
+
+    // Erro
+    else {
+        mensagem.innerHTML = "Usuário ou senha inválidos! Tente novamente.";
+    }
+}
